2317e3cb6d3003572c71f1ea5f6647b9f92f69d4
Dmitry Zlotin <dmitryzlotin@gmail.com>
2019-08-05 19:42:47 +0300

    refactoring Gcd

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -2,7 +2,28 @@
 
 namespace BrainGames\Games\Gcd;
 
-function getQuestion()
+use BrainGames\Engine;
+
+function run($gamesCount)
+{
+    $name = Engine\startGame(getRegulations());
+    $flag = true;
+    for (; $gamesCount && $flag; $gamesCount--) {
+        [$task, $correctAnswer] = getTask();
+        $answer = Engine\getPlayerAnswer($task);
+        if ($answer != $correctAnswer) {
+            Engine\endGame($answer, $correctAnswer, $name);
+            $flag = false;
+        } else {
+            Engine\correct();
+        }
+    }
+    if ($flag) {
+        Engine\endGame($name);
+    }
+}
+
+function getTask()
 {
     $min = 1;
     $max = 100;
@@ -10,25 +31,18 @@ function getQuestion()
     $second = rand($min, $max);
     $quest = "{$first} {$second}";
     $correctAnswer = getDivisor($first, $second);
-    return ['question' => $quest, 'correctAnswer' => $correctAnswer];
+    return [$quest, $correctAnswer];
 }
 
-function getSpecification()
+function getRegulations()
 {
-    $specification = ['regulations' => "Find the greatest common divisor of given numbers",
-                        'quests' => []];
-    for ($i = 0; $i < 3; $i++) {
-        $fun = getQuestion();
-        $specification['quests'][$i] = $fun;
-    }
-
-    return $specification;
+    return "Find the greatest common divisor of given numbers";
 }
 
 function getDivisor($first, $second)
 {
     $result = 0;
-    for ($i = 1; $i < $first / $i && $i < $second / $i; $i++) {
+    for ($i = 1; $i < $first && $i < $second; $i++) {
         if ($first % $i == 0 && $second % $i == 0) {
             $result = $i;
         }
